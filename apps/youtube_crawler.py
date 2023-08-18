0f9536844c054686521c8e4ead61778cba043761
AnHai <anbh.1704@gmail.com>
2023-08-18 16:58:41 +0700

    anbh2/remove_sensitive_data

--- apps/youtube_crawler.py
+++ apps/youtube_crawler.py
@@ -19,11 +19,10 @@ class DetailCrawler:
         self.driver = driver
 
     def scroll_down_action(self, amount):
+        body = self.driver.find_element("tag name", "body")
         for _ in range(amount):
-            self.driver.execute_script(
-                "window.scrollTo(0, document.body.scrollHeight);"
-            )
-            time.sleep(3)
+            body.send_keys(Keys.PAGE_DOWN)
+            time.sleep(1)
 
     def extract_video_info(self):
         time.sleep(5)
@@ -149,6 +148,7 @@ class DetailCrawler:
                 year = int(new_match.group(4))
                 date = f"{day.zfill(2)}/{month.zfill(2)}/{year}"
                 return views, date
+        crawler_logger.error(f"Error at: {context}")
         return None
 
     def _extract_views_and_upload_info(self):
@@ -156,7 +156,6 @@ class DetailCrawler:
         return document.querySelector("div#bottom-row").querySelector("div#description").querySelector("tp-yt-paper-tooltip").querySelector("#tooltip").textContent
         """
         context = self.driver.execute_script(javascript_query).strip()
-        print(context)
         return self.detech_views_and_upload_info(context)
 
     def run(
@@ -166,14 +165,7 @@ class DetailCrawler:
         try:
             self.driver.get(video_url)
             time.sleep(5)
-
-            # Scroll down to load comments
-            body = self.driver.find_element("tag name", "body")
-            for _ in range(10):
-                body.send_keys(Keys.PAGE_DOWN)
-                time.sleep(1)
-
-            # Extract data
+            self.scroll_down_action(30)
             video_info = self.extract_video_info()
             comments = self.extract_comments()
             data = {
@@ -197,8 +189,6 @@ class HomePageCrawler:
     def get_all_relate_video_info(self):
         self.driver.get(YOUTUBE_HOMEPAGE_URL)
         time.sleep(5)
-
-        # Scroll down to load more videos
         for _ in range(2):
             self.driver.execute_script(
                 "window.scrollTo(0, document.body.scrollHeight);"
@@ -210,7 +200,6 @@ class HomePageCrawler:
             "css selector", "a#video-title-link"
         )
 
-        # Extract link and title values from elements
         for thumbnail in video_thumbnails:
             video_info = {
                 "link": thumbnail.get_attribute("href"),
@@ -264,6 +253,10 @@ class YoutubeCrawlerTool:
             video_links = self._get_videos_links_by_keyword(keyword)
             total_video += len(video_links)
 
+            crawler_logger.info( #CHECKPOINT 1
+                    f"Total video: {total_video}"
+            )
+            
             try:
                 detail_list.append(self.script2_scrape_video(video_links, keyword))
             except Exception as e:
@@ -288,31 +281,95 @@ class YoutubeCrawlerTool:
                 crawler_logger.error(str(e))
         return data_by_keyword_dict
 
+    def script3_scrape_video(self, video_links, channel_url):
+        data_by_keyword_dict = {channel_url: []}
+        for index, video_link in enumerate(video_links):
+            try:
+                data_by_keyword_dict[channel_url].append(
+                    self.detail_crawler.run(video_link)
+                )
+                crawler_logger.info(
+                    f"Processing keyword: {channel_url} ({index + 1}/{len(video_links)})"
+                )
+            except Exception as e:
+                crawler_logger.error(str(e))
+        return data_by_keyword_dict
+    
     def _get_videos_links_by_keyword(self, keyword):
         search_url = f"https://www.youtube.com/results?search_query={keyword}"
         self.driver.get(search_url)
 
-        # Recently Update Click
-        filter_xpath = '//*[@id="chips"]/yt-chip-cloud-chip-renderer[6]'
+        try:
+            # Recently Update Click
+            filter_xpath = '//*[@id="chips"]/yt-chip-cloud-chip-renderer[6]'
+            video_filter = self.driver.find_element(By.XPATH, filter_xpath)
+            video_filter.click()
+            time.sleep(3)
+            self.detail_crawler.scroll_down_action(100)
+            video_links_xpath = (
+                '//a[contains(@href, "/watch?v=") and not(contains(@href, "shorts"))]'
+            )
+            video_links_info = self.driver.find_elements(By.XPATH, video_links_xpath)
+            video_links = [link.get_attribute("href") for link in video_links_info]
+            return video_links
+        except Exception as e:
+            crawler_logger.error(str(e))
+            return []
+
+    def _get_videos_links_by_channel_url(self, channel_url):
+        self.driver.get(channel_url)
+        time.sleep(3)
+        filter_xpath = '//*[@id="tabsContent"]/tp-yt-paper-tab[2]'
         video_filter = self.driver.find_element(By.XPATH, filter_xpath)
         video_filter.click()
-
-        time.sleep(5)
-
+        self.detail_crawler.scroll_down_action(50)
+        time.sleep(3)
         video_links_xpath = (
             '//a[contains(@href, "/watch?v=") and not(contains(@href, "shorts"))]'
         )
         video_links_info = self.driver.find_elements(By.XPATH, video_links_xpath)
         video_links = [link.get_attribute("href") for link in video_links_info]
         return video_links
-
+    
     def _export_to_json(self, detail_list):
         detail_json_string = json.dumps(detail_list, indent=4)
         with open(JSON_PATH, "w", encoding="utf-8") as json_file:
             json_file.write(detail_json_string)
 
+    def run_script3(self, channel_url_list=[]):
+        """
+        Crawl video của từng channel
+        Input: List chứa các Channel 
+        """
+        total_video = 0
+        detail_list = []
+        for channel_url in channel_url_list:
+            video_links = self._get_videos_links_by_channel_url(channel_url)
+            total_video += len(video_links)
+
+            crawler_logger.info( 
+                    f"Total video: {total_video}"
+            )
+            try:
+                detail_list.append(self.script3_scrape_video(video_links, channel_url))
+            except Exception as e:
+                crawler_logger.error((str(e)))
+
+        result = {"total_video": total_video, "youtube_data": detail_list}
+        crawler_logger.info(f"Total Video: {total_video}")
+        self._export_to_json(result)
+        self.driver.quit()
 
 if __name__ == "__main__":
     tool = YoutubeCrawlerTool()
     # tool.run_script1()
-    tool.run_script2(["Hieuthuhai", "Rap Viet", "Gerdnang", "Amee"])
+    # tool.run_script2([
+    #     "Amee",
+    #     "Amme", "Bray", "Goku", "Liu Grace"
+    #     "Andree", "MCK"
+    # ])
+    tool.run_script3([
+        "https://www.youtube.com/channel/UCe8b9jSSD-bNabF4hkNN5PQ",
+        "https://www.youtube.com/@Sontungmtp",
+        "https://www.youtube.com/@VieTalents"
+    ])
